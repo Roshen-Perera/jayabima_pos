1ecb5a7a1d5936cb39e80093b8b9a922f0894e11
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-12 11:54:15 +0530

    feat: update DynamicTitle component to display dynamic titles based on navigation items

--- components/DynamicTitle.tsx
+++ components/DynamicTitle.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 const DynamicTitle = () => {
   return (
     <div>
-      
+      <h1 className="text-base font-medium">
+        {navItems.find((item) => item.url === pathname)?.title || "Dashboard"}
+      </h1>
     </div>
-  )
+  );
 }
 
 export default DynamicTitle
