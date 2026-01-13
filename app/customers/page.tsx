49764b1148774e80ac91cdd96a06f651faf1f0f5
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-13 10:13:52 +0530

    refactor: comment out unused customer data calculations

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -12,11 +12,11 @@ import {
 import React from "react";
 
 const page = () => {
-  const newCustomers = customers.slice(-5); // Assuming last 5 are new customers
-  const totalCredit = customers.reduce((sum, c) => sum + c.creditBalance, 0);
-  const outStandingCustomers = customers.filter(
-    (c) => c.creditBalance > 0
-  ).length;
+  // const newCustomers = customers.slice(-5); // Assuming last 5 are new customers
+  // const totalCredit = customers.reduce((sum, c) => sum + c.creditBalance, 0);
+  // const outStandingCustomers = customers.filter(
+  //   (c) => c.creditBalance > 0
+  // ).length;
 
   return (
     <div className="flex flex-col gap-3">
