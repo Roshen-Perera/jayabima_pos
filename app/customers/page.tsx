8e75e94fbe677b390efe3c8b2d1e7655c98401a3
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-13 12:07:39 +0530

    feat: add CustomerSearch component to the customer page

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import CustomerHeader from "./components/CustomerHeader";
 import CustomerList from "./components/CustomerList";
 import CustomerStats from "./components/CustomerStats";
+import CustomerSearch from "./components/CustomerSearch";
 
 const page = () => {
   return (
     <div className="flex flex-col gap-3">
       <CustomerHeader />
       <CustomerStats />
+      <CustomerSearch />
       <CustomerList />
     </div>
   );
