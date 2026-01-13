de18eb6b90b6f87a7ccdce9065f3b08d5af6382c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-13 12:18:08 +0530

    feat: add Customer interface to define customer properties

---
+++ app/customers/types/customer.types.ts
@@ -0,0 +1,9 @@
+export interface Customer {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    creditBalance: number;
+    loyaltyPoints: number;
+    totalPurchases: number;
+}
