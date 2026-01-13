81dca3917dfcd55ae58e11eff8aab406241e3974
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-13 10:27:24 +0530

    fix: update import path for Button in CustomerHeader component

--- app/customers/components/CustomerHeader.tsx
+++ app/customers/components/CustomerHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Button } from './ui/button';
 import { Download, Plus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const CustomerHeader = () => {
   return (
