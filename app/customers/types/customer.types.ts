2b073cd3b739f40dccac417b1e657fc270ffce6a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-13 21:28:13 +0530

    fix: make Customer properties optional in types definition

--- app/customers/types/customer.types.ts
+++ app/customers/types/customer.types.ts
@@ -1,9 +1,10 @@
 export interface Customer {
-    id: string;
-    name: string;
-    email: string;
-    phone: string;
-    creditBalance: number;
-    loyaltyPoints: number;
-    totalPurchases: number;
+    id?: string;
+    name?: string;
+    email?: string;
+    phone?: string;
+    address?: string;
+    creditBalance?: number;
+    loyaltyPoints?: number;
+    totalPurchases?: number;
 }
