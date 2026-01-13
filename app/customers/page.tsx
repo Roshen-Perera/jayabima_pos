bcd5c436846b4a43db4947c037c8d826f08d0c80
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-13 10:10:16 +0530

    refactor: remove hardcoded customer data from customer page

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -11,54 +11,6 @@ import {
 } from "lucide-react";
 import React from "react";
 
-const customers = [
-  {
-    id: "1",
-    name: "Mahesh Silva",
-    phone: "0771234567",
-    email: "mahesh@gmail.com",
-    creditBalance: 45000,
-    loyaltyPoints: 1250,
-    totalPurchases: 285000,
-  },
-  {
-    id: "2",
-    name: "Nimal Fernando",
-    phone: "0772345678",
-    email: "nimal@gmail.com",
-    creditBalance: 0,
-    loyaltyPoints: 890,
-    totalPurchases: 156000,
-  },
-  {
-    id: "3",
-    name: "Sunil Perera",
-    phone: "0773456789",
-    email: "sunil@gmail.com",
-    creditBalance: 12500,
-    loyaltyPoints: 2100,
-    totalPurchases: 420000,
-  },
-  {
-    id: "4",
-    name: "Ranjith Kumar",
-    phone: "0774567890",
-    email: "ranjith@gmail.com",
-    creditBalance: 78000,
-    loyaltyPoints: 560,
-    totalPurchases: 95000,
-  },
-  {
-    id: "5",
-    name: "Kamal Jayasinghe",
-    phone: "0775678901",
-    email: "kamal@gmail.com",
-    creditBalance: 0,
-    loyaltyPoints: 3200,
-    totalPurchases: 650000,
-  },
-];
-
 const page = () => {
   const newCustomers = customers.slice(-5); // Assuming last 5 are new customers
   const totalCredit = customers.reduce((sum, c) => sum + c.creditBalance, 0);
