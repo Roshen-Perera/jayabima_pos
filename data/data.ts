397ff6d6bbb244eaf5e15b69d207e4d8e6788762
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-13 12:24:19 +0530

    fix: rename customers array to dummyCustomers for clarity

--- data/data.ts
+++ data/data.ts
@@ -1,6 +1,6 @@
 import { Customer } from "@/app/customers/types/customer.types";
 
-export const customers: Customer[] = [
+export const dummyCustomers: Customer[] = [
     { id: '1', name: 'Mahesh Silva', phone: '0771234567', email: 'mahesh@gmail.com', creditBalance: 45000, loyaltyPoints: 1250, totalPurchases: 285000},
     { id: '2', name: 'Nimal Fernando', phone: '0772345678', email: 'nimal@gmail.com', creditBalance: 0, loyaltyPoints: 890, totalPurchases: 156000},
     { id: '3', name: 'Sunil Perera', phone: '0773456789', email: 'sunil@gmail.com', creditBalance: 12500, loyaltyPoints: 2100, totalPurchases: 420000},
