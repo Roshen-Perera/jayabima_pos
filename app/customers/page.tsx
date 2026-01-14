bf08cf50088d6b74a4ed9b3f085b31fb1fe4bc69
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-15 01:35:32 +0530

    feat: integrate zustand for customer state management in Page component

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -5,9 +5,15 @@ import CustomerHeader from "./components/CustomerHeader";
 import CustomerList from "./components/CustomerList";
 import CustomerStats from "./components/CustomerStats";
 import CustomerSearch from "./components/CustomerSearch";
+import { useCustomerStore } from "@/store/customerStore";
 
 const Page = () => {
-    
+    const customers = useCustomerStore((state) => state.customers);
+    const filteredCustomers = useCustomerStore((state) =>
+      state.getFilteredCustomers()
+    );
+    const search = useCustomerStore((state) => state.search);
+    const setSearch = useCustomerStore((state) => state.setSearch);
   return (
     <div className="flex flex-col gap-3">
       <CustomerHeader />
