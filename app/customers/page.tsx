3112d90198f26bb83e0146adb6ecdac0af9b7587
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-15 01:38:14 +0530

    refactor: remove direct state access from Page component

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -8,12 +8,6 @@ import CustomerSearch from "./components/CustomerSearch";
 import { useCustomerStore } from "@/store/customerStore";
 
 const Page = () => {
-    const customers = useCustomerStore((state) => state.customers);
-    const filteredCustomers = useCustomerStore((state) =>
-      state.getFilteredCustomers()
-    );
-    const search = useCustomerStore((state) => state.search);
-    const setSearch = useCustomerStore((state) => state.setSearch);
   return (
     <div className="flex flex-col gap-3">
       <CustomerHeader />
