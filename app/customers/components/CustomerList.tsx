ab58a9f496377bd47ce5c7c1a3616bd2560d3ac2
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-20 10:15:37 +0530

    feat: update CustomerList to use getFilteredCustomers for state management

--- app/customers/components/CustomerList.tsx
+++ app/customers/components/CustomerList.tsx
@@ -5,7 +5,8 @@ import { Card, CardContent } from "@/components/ui/card";
 import { useCustomerStore } from "@/store/customerStore";
 
 const CustomerList = () => {
-  const customers = useCustomerStore((state) => state.customers);
+  const customers = useCustomerStore((state) => state.getFilteredCustomers());
+
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
