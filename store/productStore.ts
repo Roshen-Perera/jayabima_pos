a5bbf848e9c87d85118567b4347c354fa5db14fe
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-24 01:10:02 +0530

    feat: Implement ProductStore interface for product management

---
+++ store/productStore.ts
@@ -0,0 +1,16 @@
+import { Product } from "@/app/inventory/_types/product.types";
+
+
+interface ProductStore {
+    products: Product[];
+    search: string;
+    categoryFilter: string;
+
+    setProducts: (products: Product[]) => void;
+    addProduct: (product: Omit<Product, 'id' | 'createdAt' | 'updatedAt'>) => void;
+    updateProduct: (id: string, updates: Partial<Product>) => void;
+    deleteProduct: (id: string) => void;
+    updateStock: (id: string, quantity: number) => void;
+    setSearch: (search: string) => void;
+    setCategoryFilter: (category: string) => void;
+}
