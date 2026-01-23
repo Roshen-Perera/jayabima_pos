db2ce48ce9c9acbb6eeb3b895ce592fd7fe6a697
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-23 16:16:32 +0530

    feat: Added Product Header Component to page.tsx

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import ProductHeader from "./_components/ProductHeader";
 
 const page = () => {
   return (
     <>
-      <div className="flex flex-col gap-3"></div>
+      <div className="flex flex-col gap-3">
+        <ProductHeader />
+      </div>
     </>
   );
 };
