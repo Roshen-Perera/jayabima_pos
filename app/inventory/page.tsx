f425cd585bbb1a84d596f22802aeb2241645a98d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-24 01:23:28 +0530

    feat: Add ProductStats component to inventory page for enhanced product display

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductHeader from "./_components/ProductHeader";
+import ProductStats from "./_components/ProductStats";
 
 const page = () => {
   return (
     <>
       <div className="flex flex-col gap-3">
         <ProductHeader />
+        <ProductStats />
       </div>
     </>
   );
