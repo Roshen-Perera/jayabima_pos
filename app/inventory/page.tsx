1cac6cdc80de9fb64fbfe1ab97133ac2c1b9aa11
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 19:41:28 +0530

    feat: Add ProductSearch and ProductList components to inventory page

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+
+
 import ProductHeader from "./_components/ProductHeader";
 import ProductStats from "./_components/ProductStats";
+import ProductSearch from "./_components/ProductSearch";
+import ProductList from "./_components/ProductList";
 
 const page = () => {
   return (
@@ -8,6 +11,8 @@ const page = () => {
       <div className="flex flex-col gap-3">
         <ProductHeader />
         <ProductStats />
+        <ProductSearch />
+        <ProductList />
       </div>
     </>
   );
