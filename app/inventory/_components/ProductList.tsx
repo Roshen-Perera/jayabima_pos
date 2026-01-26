73e9af9bc051d8ff39fc3000a3310703a145233d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 19:38:19 +0530

    feat: Implement search filtering in ProductList component

--- app/inventory/_components/ProductList.tsx
+++ app/inventory/_components/ProductList.tsx
@@ -1,14 +1,26 @@
-import { useProductStore } from '@/store/productStore';
-import React from 'react'
+import { useProductStore } from "@/store/productStore";
+import React from "react";
 
 const ProductList = () => {
   const products = useProductStore((s) => s.products);
   const search = useProductStore((s) => s.search);
-  return (
-    <div>
-    
-    </div>
-  )
-}
 
-export default ProductList
+  const filteredProducts = React.useMemo(() => {
+    let filtered = products;
+
+    // Search filter
+    if (search) {
+      const keyword = search.toLowerCase();
+      filtered = filtered.filter(
+        (p) =>
+          p.name.toLowerCase().includes(keyword) ||
+          p.sku.toLowerCase().includes(keyword) ||
+          p.category.toLowerCase().includes(keyword),
+      );
+    }
+    return filtered;
+  }, [products, search]);
+  return <div></div>;
+};
+
+export default ProductList;
