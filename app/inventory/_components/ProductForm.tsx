219ef529006a0ef923a18bb4f02866e67e4650ae
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 19:51:59 +0530

    refactor: Move ProductFormProps interface definition inside the ProductForm component

--- app/inventory/_components/ProductForm.tsx
+++ app/inventory/_components/ProductForm.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Product } from "../_types/product.types";
 
+interface ProductFormProps {
+  product?: Product;
+  mode: "add" | "edit";
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+}
+
 const ProductForm = () => {
-  interface ProductFormProps {
-    product?: Product;
-    mode: "add" | "edit";
-    open?: boolean;
-    onOpenChange?: (open: boolean) => void;
-  }
   return <div></div>;
 };
 
