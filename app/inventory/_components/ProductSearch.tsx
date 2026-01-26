cac2f59aba0454b7ff0ef92a8d350f4c7b5eb2b1
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 21:16:47 +0530

    style: Update layout of ProductSearch component for improved spacing

--- app/inventory/_components/ProductSearch.tsx
+++ app/inventory/_components/ProductSearch.tsx
@@ -12,7 +12,7 @@ const ProductSearch = () => {
 
   return (
     <>
-      <div>
+      <div className="flex flex-col gap-3">
         <div className="relative max-w-md">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-muted-foreground" />
           <input
