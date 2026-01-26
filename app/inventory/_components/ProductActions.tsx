2a2091935c957905b6d323bc1c2b9451363d130d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 21:26:39 +0530

    feat: Integrate useProductStore in ProductActions for delete functionality

--- app/inventory/_components/ProductActions.tsx
+++ app/inventory/_components/ProductActions.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Product } from "../_types/product.types";
+import { useProductStore } from "@/store/productStore";
 
 interface ProductActionsProps {
   product: Product;
 }
 
-const ProductActions = ({product}: ProductActionsProps) => {
+const ProductActions = ({ product }: ProductActionsProps) => {
+  const deleteProduct = useProductStore((s) => s.deleteProduct);
   return <div></div>;
 };
 
