327060ce56a041ecedb2c9ae9bf78245bd4db708
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 21:14:19 +0530

    feat: Add category filter state management to ProductSearch component

--- app/inventory/_components/ProductSearch.tsx
+++ app/inventory/_components/ProductSearch.tsx
@@ -5,6 +5,8 @@ import React from "react";
 const ProductSearch = () => {
   const search = useProductStore((s) => s.search);
   const setSearch = useProductStore((s) => s.setSearch);
+  const categoryFilter = useProductStore((s) => s.categoryFilter);
+  const setCategoryFilter = useProductStore((s) => s.setCategoryFilter);
 
   return (
     <>
