8c346ff7eb8f66401c2c1b18c297b9bc81e38446
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 21:26:57 +0530

    feat: Add state management for delete alert and edit dialog in ProductActions component

--- app/inventory/_components/ProductActions.tsx
+++ app/inventory/_components/ProductActions.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Product } from "../_types/product.types";
 import { useProductStore } from "@/store/productStore";
 
@@ -8,6 +8,8 @@ interface ProductActionsProps {
 
 const ProductActions = ({ product }: ProductActionsProps) => {
   const deleteProduct = useProductStore((s) => s.deleteProduct);
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+  const [showEditDialog, setShowEditDialog] = useState(false);
   return <div></div>;
 };
 
