6d56aaa5f240a9ff0aaa16876acb03f653cb3e71
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 21:27:26 +0530

    feat: Implement delete functionality in ProductActions component with success alert

--- app/inventory/_components/ProductActions.tsx
+++ app/inventory/_components/ProductActions.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Product } from "../_types/product.types";
 import { useProductStore } from "@/store/productStore";
+import { alert } from "@/lib/alert";
 
 interface ProductActionsProps {
   product: Product;
@@ -10,6 +11,11 @@ const ProductActions = ({ product }: ProductActionsProps) => {
   const deleteProduct = useProductStore((s) => s.deleteProduct);
   const [showDeleteAlert, setShowDeleteAlert] = useState(false);
   const [showEditDialog, setShowEditDialog] = useState(false);
+
+  const handleDelete = () => {
+    deleteProduct(product.id);
+    alert.success("Product deleted!", `${product.name} has been removed.`);
+  };
   return <div></div>;
 };
 
