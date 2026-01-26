be9305f84b577236652caf2c385d4b9345b2a33a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-26 19:56:51 +0530

    feat: Integrate product store for add and update functionality in ProductForm component

--- app/inventory/_components/ProductForm.tsx
+++ app/inventory/_components/ProductForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Product } from "../_types/product.types";
+import { useProductStore } from "@/store/productStore";
 
 interface ProductFormProps {
   product?: Product;
@@ -18,6 +19,8 @@ const ProductForm = ({
 
   const open = externalOpen !== undefined ? externalOpen : internalOpen;
   const setOpen = externalOnOpenChange || setInternalOpen;
+  const addProduct = useProductStore((state) => state.addProduct);
+  const updateProduct = useProductStore((state) => state.updateProduct);
 
   return <div></div>;
 };
