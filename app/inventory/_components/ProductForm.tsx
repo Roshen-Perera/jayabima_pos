5c76315877c48f07f823a66ce713d6b8800c0227
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-28 13:28:49 +0530

    feat: Update discount percentage calculation to retain two decimal places in ProductForm

--- app/inventory/_components/ProductForm.tsx
+++ app/inventory/_components/ProductForm.tsx
@@ -116,7 +116,7 @@ const ProductForm = ({
       if (product.price > 0 && product.cost > 0) {
         const calculatedDiscount =
           ((product.price - product.cost) / product.price) * 100;
-        setDiscountPercentage(Math.round(calculatedDiscount * 10) / 10);
+        setDiscountPercentage(Math.round(calculatedDiscount * 100) / 100);
       }
     } else if (open && !product) {
       reset({
