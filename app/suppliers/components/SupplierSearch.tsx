733d9787837e6173fcff6618159846ef0ff6bf1e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 14:52:16 +0530

    fix: Clean up formatting and remove unnecessary lines in SupplierSearch component

--- app/suppliers/components/SupplierSearch.tsx
+++ app/suppliers/components/SupplierSearch.tsx
@@ -1,14 +1,10 @@
-import { useSupplierStore } from '@/store/supplierStore';
-import React from 'react'
+import { useSupplierStore } from "@/store/supplierStore";
+import React from "react";
 
 const SupplierSearch = () => {
+  const search = useSupplierStore((s) => s.search);
 
-    const search = useSupplierStore((s) => s.search);
-    
+  return <div>SupplierSearch</div>;
+};
 
-  return (
-    <div>SupplierSearch</div>
-  )
-}
-
-export default SupplierSearch
+export default SupplierSearch;
