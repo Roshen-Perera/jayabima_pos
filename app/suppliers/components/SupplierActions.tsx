2eb72e0c99c407e0b7fbe7618f49a42836b139c7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 21:09:16 +0530

    feat: Implement delete functionality in SupplierActions component

--- app/suppliers/components/SupplierActions.tsx
+++ app/suppliers/components/SupplierActions.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Supplier } from "../types/supplier.types";
 import { useSupplierStore } from "@/store/supplierStore";
+import { alert } from "@/lib/alert";
 
 interface SupplierActionsProps {
   supplier: Supplier;
@@ -10,6 +11,11 @@ const SupplierActions = ({ supplier }: SupplierActionsProps) => {
   const deleteSupplier = useSupplierStore((state) => state.deleteSupplier);
   const [showDeleteAlert, setShowDeleteAlert] = React.useState(false);
   const [showEditDialog, setShowEditDialog] = React.useState(false);
+
+  const handleDelete = () => {
+    deleteSupplier(supplier.id);
+    alert.success("Supplier deleted!", `${supplier.name} has been removed.`);
+  };
 
   return <div>SupplierActions</div>;
 };
