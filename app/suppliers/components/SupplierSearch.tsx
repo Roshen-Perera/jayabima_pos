3d84cbe2bbd213201aa68341d3be96811d69aaed
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 14:51:56 +0530

    feat: Integrate supplier search functionality in SupplierSearch component

--- app/suppliers/components/SupplierSearch.tsx
+++ app/suppliers/components/SupplierSearch.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useSupplierStore } from '@/store/supplierStore';
+import React, { use } from 'react'
 
 const SupplierSearch = () => {
+
+    const search = useSupplierStore((s) => s.search);
+    
+
   return (
     <div>SupplierSearch</div>
   )
