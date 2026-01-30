df040e27a2904e96f0be0c0e80716438e3454ac7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 14:37:36 +0530

    feat: Enhance SupplierStats component to calculate and return supplier statistics

--- app/suppliers/components/SupplierStats.tsx
+++ app/suppliers/components/SupplierStats.tsx
@@ -3,8 +3,17 @@ import React from "react";
 
 const SupplierStats = () => {
   const suppliers = useSupplierStore((s) => s.suppliers);
+  const stats = React.useMemo(() => {
+    const totalSuppliers = suppliers.length;
+    const activeSuppliers = suppliers.filter((s) => s.active).length;
+    const inactiveSuppliers = suppliers.filter((s) => !s.active).length;
 
-  
+    return {
+      totalSuppliers,
+      activeSuppliers,
+      inactiveSuppliers,
+    };
+  }, [suppliers]);
 
   return <div>SupplierStats</div>;
 };
