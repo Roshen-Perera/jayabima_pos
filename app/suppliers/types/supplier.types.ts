c65c66abebb01439bc52f74dc5b080a5da2739c3
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 19:52:54 +0530

    fix: Correct active property type in Supplier interface

--- app/suppliers/types/supplier.types.ts
+++ app/suppliers/types/supplier.types.ts
@@ -5,7 +5,7 @@ export interface Supplier {
     email: string;
     phone: string;
     address: string;
-    active?[]: boolean;
+    active?: boolean;
     createdAt: Date;
     updatedAt: Date;
 }
