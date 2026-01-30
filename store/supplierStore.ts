f3f4177009bb0bf8fc0a228f4f4d1a6cedc4bdc1
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 14:25:25 +0530

    feat: Enhance supplierStore with Zustand for state management

--- store/supplierStore.ts
+++ store/supplierStore.ts
@@ -1,7 +1,16 @@
+import { create } from 'zustand';
 import { Supplier } from './../app/suppliers/types/supplier.types';
+import { dummySuppliers } from '@/data/data';
 
 interface SupplierStore {
     suppliers: Supplier[];
 
     setSuppliers: (suppliers: Supplier[]) => void;
 }
+
+
+export const useSupplierStore = create<SupplierStore>()((set) => ({
+    suppliers: dummySuppliers,
+
+    setSuppliers: (suppliers) => set({ suppliers }),
+}));
