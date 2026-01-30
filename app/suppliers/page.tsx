3274d07ec5e53cb78643ef85dc4e5ebb8a5d209b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 20:14:26 +0530

    feat: Add SupplierHeader component to the suppliers page

--- app/suppliers/page.tsx
+++ app/suppliers/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import SupplierStats from "./components/SupplierStats";
 import SupplierSearch from "./components/SupplierSearch";
 import SupplierList from "./components/SupplierList";
+import SupplierHeader from "./components/SupplierHeader";
 
 const page = () => {
   return (
     <div className="flex flex-col gap-3">
+      <SupplierHeader />
       <SupplierStats />
       <SupplierSearch />
       <SupplierList />
