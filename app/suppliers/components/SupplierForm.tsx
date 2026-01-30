dac177ccf79bc94c76702ca36fe3f4fd3a62dc5e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 19:44:54 +0530

    feat: Refactor SupplierForm component and ensure proper formatting

--- app/suppliers/components/SupplierForm.tsx
+++ app/suppliers/components/SupplierForm.tsx
@@ -1,8 +1,14 @@
 import { Supplier } from "../types/supplier.types";
 
+import React from "react";
+
 interface SupplierFormProps {
-    supplier?: Supplier;
-    mode: "add" | "edit";
-    open: boolean;
-    onOpenChange: (open: boolean) => void;
+  supplier?: Supplier;
+  mode: "add" | "edit";
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
 }
+
+export const SupplierForm = () => {
+  return <div>SupplierForm</div>;
+};
