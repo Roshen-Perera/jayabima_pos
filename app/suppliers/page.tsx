101cab99e48a418fddaf02d69bf0da47364e808a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-01-30 15:01:52 +0530

    feat: Add SupplierList component to the suppliers page

--- app/suppliers/page.tsx
+++ app/suppliers/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SupplierStats from "./components/SupplierStats";
 import SupplierSearch from "./components/SupplierSearch";
+import SupplierList from "./components/SupplierList";
 
 const page = () => {
   return (
     <div className="flex flex-col gap-3">
       <SupplierStats />
       <SupplierSearch />
+      <SupplierList />
     </div>
   );
 };
