f23e126c22dbeb51bae9c7493e999cd214b31bac
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-05 12:38:11 +0530

    feat: refactor password hashing function and ensure SALT_ROUNDS constant is defined

--- lib/auth/password.ts
+++ lib/auth/password.ts
@@ -1,4 +1,7 @@
 import bcrypt from 'bcryptjs';
 
-
 const SALT_ROUNDS = 12; // The cost factor controls how much time is needed to calculate a single BCrypt hash.
+
+export async function hashPassword(password: string){
+    return await bcrypt.hash(password, SALT_ROUNDS);
+}
