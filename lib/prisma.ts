6c6ea50b9f5753dc17f08e1526dbfa5d1f5edebb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-05 12:30:57 +0530

    feat: ensure Prisma client is set in global scope for non-production environments

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,3 +9,5 @@ export const prisma =
     new PrismaClient({
         log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
     });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
