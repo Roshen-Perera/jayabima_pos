c0452460265871a3c261bbd582ae80b7da4d3bfd
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-06 11:16:28 +0530

    feat: implement error handling in POST function for forgot password route

--- app/api/auth/forgot-password/route.ts
+++ app/api/auth/forgot-password/route.ts
@@ -2,11 +2,16 @@ import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
 import prisma from '@/lib/prisma';
 import { generateResetToken } from '@/lib/auth/jwt';
+import { ca } from 'zod/v4/locales';
 
 const forgotPasswordSchema = z.object({
     email: z.string().email('Invalid email address'),
 });
 
 export async function POST(request: NextRequest) {
-    
+    try {
+        const body = await request.json();
+    } catch (error) {
+        
+    }
 }
