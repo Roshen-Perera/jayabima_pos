2f07b6916866a325d25733949e1ccaca2028fa93
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-09 14:26:06 +0530

    feat: add session validation in GET function to ensure user authentication

--- app/api/auth/my-account/route.ts
+++ app/api/auth/my-account/route.ts
@@ -1,9 +1,19 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
+import { getCurrentUser } from '@/lib/auth/session';
 
 export async function GET() {
     try {
-
+        const session = await getCurrentUser();
+        if (!session) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: 'Not authenticated',
+                },
+                { status: 401 }
+            );
+        }
     } catch (error) {
 
     }
