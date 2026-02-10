806dadd7ec57f79e7370b93820adeb4b3f7bc63d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-10 13:36:33 +0530

    feat: initialize useAuthStore with zustand for state management

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -2,3 +2,5 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import type { AuthState, LoginCredentials, RegisterData, AuthResponse, AuthUser } from '@/types/auth.types';
 
+export const useAuthStore = create<AuthState>()(
+)
