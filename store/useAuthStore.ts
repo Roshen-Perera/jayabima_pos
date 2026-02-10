b52339d5d24f5c98db2136c2927a67ced9c8b303
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-10 13:36:49 +0530

    feat: implement persist middleware in useAuthStore for state persistence

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -3,4 +3,8 @@ import { persist } from 'zustand/middleware';
 import type { AuthState, LoginCredentials, RegisterData, AuthResponse, AuthUser } from '@/types/auth.types';
 
 export const useAuthStore = create<AuthState>()(
+    persist(
+        
+    )
+
 )
