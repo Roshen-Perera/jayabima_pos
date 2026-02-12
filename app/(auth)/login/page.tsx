a563533d5e970a9a2bffdf4f5764d56cb21915b6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-12 12:03:34 +0530

    feat: add structure to login page component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,7 +9,9 @@ const page = () => {
   return (
     <div>
       <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 p-4">
-        
+        <div className="w-full max-w-md">
+            
+        </div>
       </div>
     </div>
   );
