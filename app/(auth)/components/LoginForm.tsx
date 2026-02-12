09bd35dc8f7f20046903944bb75e215d406d7a1d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-12 10:53:12 +0530

    feat: add initial implementation of LoginForm component

---
+++ app/(auth)/components/LoginForm.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const LoginForm = () => {
+  const router = useRouter();
+  return <></>;
+};
+
+export default LoginForm;
