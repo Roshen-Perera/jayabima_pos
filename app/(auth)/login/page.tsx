0bda21e45f5f476e7bc1095cff99ca5a844e1390
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-12 12:02:35 +0530

    feat: add metadata for login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata = {
+  title: "Login | JAYABIMA POS",
+  description: "Sign in to your JAYABIMA POS account",
+};
+
 const page = () => {
   return (
     <div>page</div>
