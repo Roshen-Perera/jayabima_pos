33ef367a0c4789202aa1218ebdd3737abc935a95
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-12 10:53:29 +0530

    feat: integrate login function from auth store into LoginForm component

--- app/(auth)/components/LoginForm.tsx
+++ app/(auth)/components/LoginForm.tsx
@@ -1,8 +1,10 @@
+import { useAuthStore } from "@/store/useAuthStore";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 const LoginForm = () => {
   const router = useRouter();
+  const login = useAuthStore((state) => state.login);
   return <></>;
 };
 
