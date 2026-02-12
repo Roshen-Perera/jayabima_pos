d34782deaccb903dda85f22696fc99b800f9050a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-12 12:03:07 +0530

    fix: update formatting and structure of login page component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 export const metadata = {
   title: "Login | JAYABIMA POS",
@@ -7,8 +7,12 @@ export const metadata = {
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 p-4">
+        
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
