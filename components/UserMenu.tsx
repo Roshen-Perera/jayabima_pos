a3fd17092f6170f56b8ba21861929d18d151e351
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-13 22:58:44 +0530

    feat: integrate useAuthStore for user and logout functionality in UserMenu

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -1,8 +1,10 @@
+import { useAuthStore } from "@/store/useAuthStore";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 const UserMenu = () => {
   const router = useRouter();
+  const { user, logout } = useAuthStore();
   return <div>UserMenu</div>;
 };
 
