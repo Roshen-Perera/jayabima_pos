bfdddafe79e7e1f71946a8e22614b76968213b9c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-13 23:09:44 +0530

    refactor: remove unused user variable from Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,6 @@ import { Badge } from "./ui/badge";
 import UserMenu from "./UserMenu";
 
 const Header = () => {
-  const { user } = useAuthStore();
   return (
     <>
       <header className="flex h-15 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
