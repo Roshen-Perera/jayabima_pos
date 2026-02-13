3d218ee26bddcab28a40ca8e816974c8b00c3968
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-13 22:59:09 +0530

    feat: refactor getInitials function in UserMenu component for better readability

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -6,6 +6,15 @@ const UserMenu = () => {
   const router = useRouter();
   const { user, logout } = useAuthStore();
   const [isLoggingOut, setIsLoggingOut] = useState(false);
+
+  const getInitials = (name: string) => {
+    return name
+      .split(" ")
+      .map((n) => n[0])
+      .join("")
+      .toUpperCase()
+      .slice(0, 2);
+  };
   return <div>UserMenu</div>;
 };
 
