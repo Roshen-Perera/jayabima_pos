a48a6557ba843430fde66e6d200a8a039ce87249
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-13 22:58:56 +0530

    feat: add logout state management to UserMenu component

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -1,10 +1,11 @@
 import { useAuthStore } from "@/store/useAuthStore";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const UserMenu = () => {
   const router = useRouter();
   const { user, logout } = useAuthStore();
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
   return <div>UserMenu</div>;
 };
 
