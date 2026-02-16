592816d7e49a35cc9039902c295818e9888fb4b2
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-17 00:31:37 +0530

    feat: define CartItemProps interface and update CartItem component to accept props

--- app/pos/_components/CartItem.tsx
+++ app/pos/_components/CartItem.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { CartItem as CartItemType } from '../_types/pos.types';
 
-const CartItem = () => {
+interface CartItemProps {
+  item: CartItemType;
+  onUpdateQuantity: (productId: string, quantity: number) => void;
+  onRemove: (productId: string) => void;
+}
+
+
+const CartItem = ({ item, onUpdateQuantity, onRemove }: CartItemProps) => {
   return (
     <div>CartItem</div>
   )
