898526badef8b4e144366a53a208bbb63dfd7d46
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-17 19:02:36 +0530

    feat: define CheckoutPanelProps interface for CheckoutPanel component

--- app/pos/_components/CheckoutPanel.tsx
+++ app/pos/_components/CheckoutPanel.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const CheckoutPanel = () => {
-  return (
-    <div>CheckoutPanel</div>
-  )
+interface CheckoutPanelProps {
+  open: boolean;
+  onClose: () => void;
+  onSuccess: (saleId: string) => void;
 }
 
-export default CheckoutPanel
+const CheckoutPanel = () => {
+  return <div>CheckoutPanel</div>;
+};
+
+export default CheckoutPanel;
