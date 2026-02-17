df220cbd51a9a509bcbda77fb36a1b2a1455d002
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-17 18:56:21 +0530

    feat: add error handling structure to fetchProducts function in ProductGrid component

--- app/pos/_components/ProductGrid.tsx
+++ app/pos/_components/ProductGrid.tsx
@@ -9,10 +9,11 @@ const ProductGrid = ({ searchQuery, onAddToCart }: ProductGridProps) => {
   const [products, setProducts] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
-
   const fetchProducts = async () => {
     setIsLoading(true);
-  }
+    try {
+    } catch (error) {}
+  };
   return <div>ProductGrid</div>;
 };
 
