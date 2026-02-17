d785f324cc17de2ef411829061e3450a7178be13
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-17 20:46:42 +0530

    refactor: remove unused Image import in CartItem component

--- app/pos/_components/CartItem.tsx
+++ app/pos/_components/CartItem.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { CartItem as CartItemType } from "../_types/pos.types";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Minus, Plus, Trash2 } from "lucide-react";
 import { Input } from "@/components/ui/input";
