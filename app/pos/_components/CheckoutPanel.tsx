5e51e5fd8e472d8c81ced5e81150572f5b485ea0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-17 19:06:57 +0530

    feat: add user authentication check in handleCheckout function of CheckoutPanel component

--- app/pos/_components/CheckoutPanel.tsx
+++ app/pos/_components/CheckoutPanel.tsx
@@ -3,6 +3,7 @@ import { useAuthStore } from "@/store/useAuthStore";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { PaymentMethod } from "../_types/pos.types";
+import { alert } from "@/lib/alert";
 
 interface CheckoutPanelProps {
   open: boolean;
@@ -17,7 +18,12 @@ const CheckoutPanel = ({ open, onClose, onSuccess }: CheckoutPanelProps) => {
   const [paymentMethod, setPaymentMethod] = useState<PaymentMethod>("CASH");
   const [isProcessing, setIsProcessing] = useState(false);
 
-  const handleCheckout = async () => {};
+  const handleCheckout = async () => {
+    if (!user) {
+      alert.error("Not logged in", "Please log in to process sales.");
+      return;
+    }
+  };
   return <div>CheckoutPanel</div>;
 };
 
