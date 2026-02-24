07cef8a82c5d116d6a62dd097a915d3a88caf036
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-02-24 21:36:02 +0530

    feat: implement layout structure in SalesStats component

--- app/sales/components/SalesStats.tsx
+++ app/sales/components/SalesStats.tsx
@@ -9,4 +9,8 @@ interface SalesStatsProps {
   };
 }
 
-export default function SalesStats({ stats }: SalesStatsProps) {}
+export default function SalesStats({ stats }: SalesStatsProps) {
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        
+    </div>
+}
