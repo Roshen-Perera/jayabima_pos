8921754197f66ec401b56098bc77e9a095fb5d76
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-04 12:40:11 +0530

    refactor: reorganize imports in usePermissions hook and prepare return structure

--- hooks/usePermissions.ts
+++ hooks/usePermissions.ts
@@ -1,7 +1,11 @@
-import { useAuthStore, UserRole } from "@/store/useAuthStore";
+import { UserRole } from "@/lib/rbac/permissions";
+import { useAuthStore } from "@/store/useAuthStore";
 
 
 export function usePermissions() {
     const user = useAuthStore((state) => state.user);
     const role = user?.role as UserRole;
+    return {
+        
+    }
 }
