c403dcee3f3f766024b9b5d45a21c33326b0b1eb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-04 12:57:47 +0530

    feat: integrate usePermissions hook in unauthorized page component

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -1,8 +1,10 @@
+import { usePermissions } from "@/hooks/usePermissions";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 const Page = () => {
   const router = useRouter();
+  const { role, user } = usePermissions();
   return <div>Page</div>;
 };
 
