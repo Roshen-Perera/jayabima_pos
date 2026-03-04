20e84c3411a91bd607479dffe55134b39ab0fcc6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-04 12:39:30 +0530

    feat: add UserRole type import and define role in usePermissions hook

--- hooks/usePermissions.ts
+++ hooks/usePermissions.ts
@@ -1,6 +1,7 @@
-import { useAuthStore } from "@/store/useAuthStore";
+import { useAuthStore, UserRole } from "@/store/useAuthStore";
 
 
 export function usePermissions() {
     const user = useAuthStore((state) => state.user);
+    const role = user?.role as UserRole;
 }
