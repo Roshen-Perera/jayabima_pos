d6983ab92733e8e6837a0ab0ccf7ee2dcabcc243
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-10 10:56:53 +0530

    feat: add function to generate temporary passwords for password reset

---
+++ app/api/employees/[id]/reset-password/route.ts
@@ -0,0 +1,10 @@
+
+
+function generateTempPassword(): string {
+    const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789!@#$%';
+    let password = '';
+    for (let i = 0; i < 12; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+}
