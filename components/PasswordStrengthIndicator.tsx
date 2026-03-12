63e927a0046df616088813c1b442006e7f5d0452
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-12 21:19:15 +0530

    feat: implement password strength requirements in PasswordStrengthIndicator component

--- components/PasswordStrengthIndicator.tsx
+++ components/PasswordStrengthIndicator.tsx
@@ -1,7 +1,34 @@
+import { useMemo } from "react";
+
 interface PasswordStrengthIndicatorProps {
   password: string;
 }
 
 export default function PasswordStrengthIndicator({
   password,
-}: PasswordStrengthIndicatorProps) {}
+}: PasswordStrengthIndicatorProps) {
+    const requirements = useMemo(() => {
+      return [
+        {
+          label: "At least 8 characters",
+          met: password.length >= 8,
+        },
+        {
+          label: "Contains uppercase letter",
+          met: /[A-Z]/.test(password),
+        },
+        {
+          label: "Contains lowercase letter",
+          met: /[a-z]/.test(password),
+        },
+        {
+          label: "Contains number",
+          met: /[0-9]/.test(password),
+        },
+        {
+          label: "Contains special character",
+          met: /[!@#$%^&*(),.?":{}|<>]/.test(password),
+        },
+      ];
+    }, [password]);
+}
