7072f8f5541a0584ab2b03c1af4bcc80f222c602
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-12 21:19:37 +0530

    refactor: reorganize requirements and strength calculation in PasswordStrengthIndicator component

--- components/PasswordStrengthIndicator.tsx
+++ components/PasswordStrengthIndicator.tsx
@@ -7,28 +7,36 @@ interface PasswordStrengthIndicatorProps {
 export default function PasswordStrengthIndicator({
   password,
 }: PasswordStrengthIndicatorProps) {
-    const requirements = useMemo(() => {
-      return [
-        {
-          label: "At least 8 characters",
-          met: password.length >= 8,
-        },
-        {
-          label: "Contains uppercase letter",
-          met: /[A-Z]/.test(password),
-        },
-        {
-          label: "Contains lowercase letter",
-          met: /[a-z]/.test(password),
-        },
-        {
-          label: "Contains number",
-          met: /[0-9]/.test(password),
-        },
-        {
-          label: "Contains special character",
-          met: /[!@#$%^&*(),.?":{}|<>]/.test(password),
-        },
-      ];
-    }, [password]);
+  const requirements = useMemo(() => {
+    return [
+      {
+        label: "At least 8 characters",
+        met: password.length >= 8,
+      },
+      {
+        label: "Contains uppercase letter",
+        met: /[A-Z]/.test(password),
+      },
+      {
+        label: "Contains lowercase letter",
+        met: /[a-z]/.test(password),
+      },
+      {
+        label: "Contains number",
+        met: /[0-9]/.test(password),
+      },
+      {
+        label: "Contains special character",
+        met: /[!@#$%^&*(),.?":{}|<>]/.test(password),
+      },
+    ];
+  }, [password]);
+
+  const strength = useMemo(() => {
+    const metCount = requirements.filter((r) => r.met).length;
+    if (metCount === 0) return { label: "", color: "" };
+    if (metCount <= 2) return { label: "Weak", color: "text-destructive" };
+    if (metCount <= 4) return { label: "Medium", color: "text-yellow-500" };
+    return { label: "Strong", color: "text-green-500" };
+  }, [requirements]);
 }
