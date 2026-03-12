a50c2c55e19974fe41198a50d452b923a014d2d4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-12 21:32:16 +0530

    feat: add state management for editing in ProfileInfoCard component

--- app/profile/_components/ProfileInfoCard.tsx
+++ app/profile/_components/ProfileInfoCard.tsx
@@ -1,10 +1,12 @@
 import { useProfileStore } from "@/store/profileStore";
 import { User } from "@/types/user.types";
+import { useState } from "react";
 
 interface ProfileInfoCardProps {
   user: User;
 }
 
 export default function ProfileInfoCard({ user }: ProfileInfoCardProps) {
-    const { updateProfile, isSubmitting } = useProfileStore();
+  const { updateProfile, isSubmitting } = useProfileStore();
+  const [isEditing, setIsEditing] = useState(false);
 }
