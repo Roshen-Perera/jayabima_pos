b1d729423cee60528927b34c2f1a0202b027b0b3
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-12 22:08:11 +0530

    feat: add loading skeletons to profile page while fetching data

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { useProfileStore } from "@/store/profileStore";
 import React, { useEffect } from "react";
 
@@ -6,6 +7,14 @@ const Page = () => {
   useEffect(() => {
     fetchProfile();
   }, [fetchProfile]);
+  if (isLoading) {
+    return (
+      <div className="container mx-auto py-6 space-y-6">
+        <Skeleton className="h-32 w-full" />
+        <Skeleton className="h-96 w-full" />
+      </div>
+    );
+  }
   return <div>Page</div>;
 };
 
