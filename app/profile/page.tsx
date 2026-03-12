9cba785cf795150061590d01e892549678f6096d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-12 22:07:46 +0530

    feat: fetch user profile data on page load

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,11 @@
 import { useProfileStore } from "@/store/profileStore";
-import React from "react";
+import React, { useEffect } from "react";
 
 const Page = () => {
   const { profile, isLoading, fetchProfile } = useProfileStore();
+  useEffect(() => {
+    fetchProfile();
+  }, [fetchProfile]);
   return <div>Page</div>;
 };
 
