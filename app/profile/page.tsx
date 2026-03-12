4f976ecc1b4b8ff354dbf35336807aca53ee5086
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-12 22:07:26 +0530

    feat: integrate profile store to fetch user profile data

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,8 @@
+import { useProfileStore } from "@/store/profileStore";
 import React from "react";
 
 const Page = () => {
+  const { profile, isLoading, fetchProfile } = useProfileStore();
   return <div>Page</div>;
 };
 
