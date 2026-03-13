51e1b77feaa0fbbafa1eeaaaa7463141fe26bedb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-13 14:10:00 +0530

    feat: define EmployeeFiltersProps interface for employee management

---
+++ app/employees/_components/EmployeeFilters.tsx
@@ -0,0 +1,10 @@
+
+
+interface EmployeeFiltersProps {
+  searchQuery: string;
+  roleFilter: string;
+  statusFilter: string;
+  onSearchChange: (value: string) => void;
+  onRoleChange: (value: string) => void;
+  onStatusChange: (value: string) => void;
+}
