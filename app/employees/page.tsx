ce462273909751c79dd554145699e0aceff8a9d3
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-13 18:45:20 +0530

    feat: implement employee data retrieval in Page component

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -1,6 +1,19 @@
+import { useEmployeeStore } from '@/store/employeeStore';
 import React from 'react'
 
 const Page = () => {
+  const {
+    employees,
+    isLoading,
+    searchQuery,
+    roleFilter,
+    statusFilter,
+    fetchEmployees,
+    setSearchQuery,
+    setRoleFilter,
+    setStatusFilter,
+    deleteEmployee,
+  } = useEmployeeStore();
   return (
     <div></div>
   )
