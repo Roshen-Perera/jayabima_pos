124a19213caef0d38403d3eba0530acb42511c70
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-13 14:17:30 +0530

    feat: implement DeleteEmployeeDialogProps interface for employee deletion

---
+++ app/employees/_components/DeleteEmployeeDialog.tsx
@@ -0,0 +1,9 @@
+import { Employee } from "@/types/employee.type";
+
+interface DeleteEmployeeDialogProps {
+  employee: Employee | null;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onConfirm: () => void;
+  isDeleting: boolean;
+}
