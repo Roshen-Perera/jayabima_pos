fc3812efb38f1a899ff97c5e3d68cbfcb952e5b1
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-17 14:48:42 +0530

    feat: add emailSent state management to forgot password page component

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -5,6 +5,7 @@ const Page = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [emailSent, setEmailSent] = useState(false);
 
   return <div></div>;
 };
