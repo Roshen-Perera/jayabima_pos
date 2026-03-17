fad59d4c2c2d6a34aec953c17f39efa9027defd2
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-17 15:25:51 +0530

    feat: retrieve token from search parameters in Reset Password component

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -3,4 +3,5 @@ import { useRouter, useSearchParams } from "next/navigation";
 function ResetPasswordContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const token = searchParams.get("token");
 }
