de710ba5703ce6afeb2685f22c425ba197868e38
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-17 15:55:36 +0530

    feat: add lock icon to new password field for enhanced UI in Reset Password form

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -165,7 +165,9 @@ function ResetPasswordContent() {
             <Label htmlFor="newPassword">
               New Password <span className="text-destructive">*</span>
             </Label>
-            <div className="relative"></div>
+            <div className="relative">
+              <Lock className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+            </div>
           </div>
         </form>
       </CardContent>
