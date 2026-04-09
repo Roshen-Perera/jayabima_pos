32b1b336ffe8330c2937bdb0c4270946d71bb2f0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-09 23:49:20 +0530

    feat: add missing newline at the end of route.ts file

--- app/api/customers/[id]/route.ts
+++ app/api/customers/[id]/route.ts
@@ -2,3 +2,4 @@ import { prisma } from '@/lib/prisma';
 import { customerSchema } from '@/app/customers/lib/validation';
 import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
+
