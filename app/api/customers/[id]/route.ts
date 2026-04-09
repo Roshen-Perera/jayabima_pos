f32f7c2126457bc4a0a94dd7da1cdf6dd41d3da1
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-09 23:49:50 +0530

    feat: implement PUT function for customer API route

--- app/api/customers/[id]/route.ts
+++ app/api/customers/[id]/route.ts
@@ -3,3 +3,7 @@ import { customerSchema } from '@/app/customers/lib/validation';
 import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
 
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {}
