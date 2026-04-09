63e580cdc8ac4172d856407ee40ac27f7c2e2824
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-09 23:38:58 +0530

    feat: implement GET function to retrieve customers from the database

--- app/api/customers/route.ts
+++ app/api/customers/route.ts
@@ -1,9 +1,12 @@
-
+import { prisma } from '@/lib/prisma';
 
 export async function GET() {
     try {
-
+        const customers = await prisma.customer.findMany({
+            where: { deletedAt: null },
+            orderBy: { createdAt: 'desc' },
+        });
     } catch (error) {
-        
+
     }
 }
