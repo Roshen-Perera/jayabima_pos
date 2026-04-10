f86f64359c0721ee35e2f9f026c8cbfa6802f817
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-10 13:07:46 +0530

    fix: refactor Page component to include useEffect for loading customers

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import CustomerHeader from "./components/CustomerHeader";
 import CustomerList from "./components/CustomerList";
 import CustomerStats from "./components/CustomerStats";
 import CustomerSearch from "./components/CustomerSearch";
+import { useCustomerStore } from "@/store/customerStore";
 
 const Page = () => {
+  const { loadCustomers, loadInactiveCustomers } = useCustomerStore();
+
+  useEffect(() => {
+    // Load fresh data when page mounts
+    loadCustomers();
+    loadInactiveCustomers();
+  }, [loadCustomers, loadInactiveCustomers]);
   return (
     <div className="flex flex-col gap-3">
       <CustomerHeader />
-      <CustomerStats  />
-      <CustomerSearch/>
-      <CustomerList/>
+      <CustomerStats />
+      <CustomerSearch />
+      <CustomerList />
     </div>
   );
 };
