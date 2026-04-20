5125cd495deb1c982c47daede8bc8f2a043f2f7a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-20 14:26:38 +0530

    feat: return updated product in response for PUT route

--- app/api/inventory/[id]/route.ts
+++ app/api/inventory/[id]/route.ts
@@ -1,5 +1,5 @@
 import { productSchema } from "@/app/inventory/lib/validation";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function PUT(
@@ -14,6 +14,7 @@ export async function PUT(
             where: { id },
             data: validatedData,
         });
+        return NextResponse.json(product);
     } catch (error) {
 
     }
