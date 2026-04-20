1fb06b5df96b50a8717d238cf5ec7d11568acf1c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-20 14:26:10 +0530

    feat: implement product update logic in PUT route for inventory

--- app/api/inventory/[id]/route.ts
+++ app/api/inventory/[id]/route.ts
@@ -1,14 +1,19 @@
 import { productSchema } from "@/app/inventory/lib/validation";
 import { NextRequest } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export async function PUT(
     request: NextRequest,
     { params }: { params: Promise<{ id: string }> }
 ) {
-    try{
+    try {
         const { id } = await params;
         const body = await request.json();
         const validatedData = productSchema.partial().parse(body);
+        const product = await prisma.product.update({
+            where: { id },
+            data: validatedData,
+        });
     } catch (error) {
 
     }
