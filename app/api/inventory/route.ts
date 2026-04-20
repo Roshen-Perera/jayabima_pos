aa45f6cb06220f8a9020d2600c3ac83c8c52d139
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-04-20 12:38:11 +0530

    feat: add searchParams extraction in GET route for inventory API

--- app/api/inventory/route.ts
+++ app/api/inventory/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-    try{
+    try {
+        const searchParams = request.nextUrl.searchParams;
 
     } catch (error) {
-        
+
     }
 }
